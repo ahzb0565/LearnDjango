35c4c29b0cc142cc2d0ed329e7e28d641c198444
Bob <ahzb0565@gmail.com>
2015-08-07 00:35:20 +0800

    use template in views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader, RequestContext
 
 # Create your views here.
 
@@ -14,3 +16,15 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s."%question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([p.question_text for p in latest_question_list])
+    #return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(
+            request,
+            {'latest_question_list': latest_question_list},
+        )
+    return HttpResponse(template.render(context))
