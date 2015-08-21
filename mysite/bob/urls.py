a78e378f981083e7aec1f04515b2c968176faee3
bozhoux <ahzb0565@gmail.com>
2015-08-21 15:37:18 +0800

    fix the error of url reverse

--- mysite/bob/urls.py
+++ mysite/bob/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     #Index, list all executed results
     url(r"^$", views.index, name = 'index'),
     #Execution Plan Result
     url(r"^(?P<plan_id>[0-9]+)/$", views.result, name = 'result'),
-    }
+    ]
