3aab6dbe8e983e53525be90fd3a6ab1ba0f03e59
bozhoux <ahzb0565@gmail.com>
2015-08-21 17:18:44 +0800

    add file upload -- inprogress

--- mysite/bob/urls.py
+++ mysite/bob/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r"^$", views.index, name = 'index'),
     #Execution Plan Result
     url(r"^(?P<plan_id>[0-9]+)/$", views.result, name = 'result'),
+    #result uploader
+    url(r"^result-upload/$", views.result_uploader, name = 'result-upload'),
+    #upload csv result file
+    url(r"upload-csv/$", views.upload_csv, name='upload-csv'),
     ]
--- mysite/bob/views.py
+++ mysite/bob/views.py
@@ -12,3 +12,10 @@ def result(request, plan_id):
 def index(request):
     plans = ExecutionPlan.objects.all()
     return render(request, 'bob/index.html',{'plans': plans})
+
+def result_uploader(request):
+    return render(request, 'bob/result_uploader.html',{})
+
+def upload_csv(request):
+    file_path = request.POST['file_path']
+    return HttpResponse("Uploading file: %s"%file_path)
